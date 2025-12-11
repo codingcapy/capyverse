ab6999afb6379a9590f2ae1fefae78a4a237f000
Paul K <102385105+codingcapy@users.noreply.github.com>
2025-12-11 11:14:35 -0800

    feat: build search toggle on mobile viewport

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -3,64 +3,87 @@ import useAuthStore from "../store/AuthStore";
 import { useState } from "react";
 import { CgProfile } from "react-icons/cg";
 import { IoSearch } from "react-icons/io5";
+import { FaArrowLeft } from "react-icons/fa";
 
 export function Header() {
   const { user } = useAuthStore();
   const [showMenu, setShowMenu] = useState(false);
   const { logoutService } = useAuthStore();
+  const [searchMode, setSearchMode] = useState(false);
 
   return (
-    <header className="fixed top-0 left-0 p-2 md:px-5 md:py-2 z-50 flex justify-between w-screen bg-[#222222] border-b border-[#808080]">
-      <Link to="/" className="text-2xl text-cyan-500 font-bold">
-        Capyverse
-      </Link>
-      {window.innerWidth > 500 && (
-        <input
-          type="text"
-          className="px-5 py-2 rounded-2xl w-[30%] bg-[#414141]"
-          placeholder="Search Capyverse"
-        />
-      )}
-      {user ? (
-        <div className="flex px-5 py-2 font-bold">
-          {window.innerWidth < 501 && (
-            <div className="cursor-pointer">
-              <IoSearch size={25} />
-            </div>
-          )}
-          <Link
-            to="/createpost"
-            className="px-5 cursor-pointer hover:text-cyan-500 transition-all ease-in-out duration-300"
-          >
-            + Create
-          </Link>
+    <div>
+      {searchMode ? (
+        <div className="fixed top-0 left-0 p-2 md:px-5 md:py-2 z-50 flex justify-between w-screen bg-[#222222] border-b border-[#808080]">
           <div
-            onClick={() => setShowMenu(!showMenu)}
-            className="flex cursor-pointer hover:text-cyan-500 transition-all ease-in-out duration-300"
+            onClick={() => setSearchMode(false)}
+            className="mt-1 cursor-pointer"
           >
-            <CgProfile size={25} />
-            <div className="ml-1">{user.username}</div>
+            <FaArrowLeft size={25} />
           </div>
+          <input
+            type="text"
+            className="px-5 py-1 ml-2 rounded-2xl w-full bg-[#414141]"
+            placeholder="Search Capyverse"
+          />
         </div>
       ) : (
-        <Link
-          to="/login"
-          className="bg-cyan-500 px-5 py-2 rounded-full font-bold"
-        >
-          Login
-        </Link>
+        <header className="fixed top-0 left-0 p-2 md:px-5 md:py-2 z-50 flex justify-between w-screen bg-[#222222] border-b border-[#808080]">
+          <Link to="/" className="text-2xl text-cyan-500 font-bold">
+            Capyverse
+          </Link>
+          {window.innerWidth > 500 && (
+            <input
+              type="text"
+              className="px-5 py-2 rounded-2xl w-[30%] bg-[#414141]"
+              placeholder="Search Capyverse"
+            />
+          )}
+          {user ? (
+            <div className="flex px-5 py-2 font-bold">
+              {window.innerWidth < 501 && (
+                <div
+                  onClick={() => setSearchMode(true)}
+                  className="cursor-pointer"
+                >
+                  <IoSearch size={25} />
+                </div>
+              )}
+              <Link
+                to="/createpost"
+                className="px-5 cursor-pointer hover:text-cyan-500 transition-all ease-in-out duration-300"
+              >
+                + Create
+              </Link>
+              <div
+                onClick={() => setShowMenu(!showMenu)}
+                className="flex cursor-pointer hover:text-cyan-500 transition-all ease-in-out duration-300"
+              >
+                <CgProfile size={25} />
+                <div className="ml-1">{user.username}</div>
+              </div>
+            </div>
+          ) : (
+            <Link
+              to="/login"
+              className="bg-cyan-500 px-5 py-2 rounded-full font-bold"
+            >
+              Login
+            </Link>
+          )}
+          {showMenu && (
+            <div
+              onClick={() => {
+                logoutService();
+                setShowMenu(false);
+              }}
+              className="absolute top-[60px] right-0 bg-[#444444] p-5 cursor-pointer hover:text-cyan-500 transition-all ease-in-out duration-300"
+            >
+              Logout
+            </div>
+          )}
+        </header>
       )}
-      {showMenu && (
-        <div
-          onClick={() => {
-            logoutService();
-            setShowMenu(false);
-          }}
-          className="absolute top-[60px] right-0 bg-[#444444] p-5 cursor-pointer hover:text-cyan-500 transition-all ease-in-out duration-300"
-        >
-          Logout
-        </div>
-      )}
-    </header>
+    </div>
   );
 }
