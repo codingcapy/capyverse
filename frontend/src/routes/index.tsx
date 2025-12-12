7d0ca90eb2fb9ad3be7e912744e47c6092705f53
PaulArkhet <spkim0921@gmail.com>
2025-12-11 16:33:38 -0800

    feat: toggle sort mode menu and reference for click outside

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -3,7 +3,7 @@ import { getPostsQueryOptions } from "../lib/api/posts";
 import { useQuery } from "@tanstack/react-query";
 import { PostThumbnail } from "../components/PostThumbnail";
 import { PiCaretDownBold } from "react-icons/pi";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 type SortMode = "Popular" | "New";
 
@@ -19,14 +19,29 @@ function IndexComponent() {
   } = useQuery(getPostsQueryOptions());
   const [showSortMenu, setShowSortMenu] = useState(false);
   const [sortMode, setSortMode] = useState<SortMode>("Popular");
+  const menuRef = useRef<HTMLDivElement | null>(null);
+
+  function handleClickOutside(event: MouseEvent) {
+    if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
+      setShowSortMenu(false);
+    }
+  }
+
+  useEffect(() => {
+    document.addEventListener("click", handleClickOutside);
+    return () => document.removeEventListener("click", handleClickOutside);
+  }, []);
 
   return (
     <div className="flex-1 p-2">
       <div className="flex flex-col">
         <div className="mx-auto w-full lg:w-[50%] pt-[70px] pb-5 ">
-          <div className="relative mx-auto w-full 2xl:w-[750px] flex">
+          <div
+            ref={menuRef}
+            className="relative mx-auto w-full 2xl:w-[750px] flex"
+          >
             <div
-              onClick={() => setShowSortMenu(true)}
+              onClick={() => setShowSortMenu(!showSortMenu)}
               className="flex cursor-pointer"
             >
               <div className="text-xs">{sortMode}</div>
@@ -37,14 +52,14 @@ function IndexComponent() {
             {showSortMenu && (
               <div
                 onClick={() => {}}
-                className="absolute top-5 left-0 bg-[#444444] p-5 z-50"
+                className="absolute top-5 left-0 bg-[#444444] px-5 py-2 z-50"
               >
                 <div
                   onClick={() => {
                     setSortMode("Popular");
                     setShowSortMenu(false);
                   }}
-                  className="cursor-pointer hover:text-cyan-500 transition-all ease-in-out duration-300"
+                  className=" py-1 cursor-pointer hover:text-cyan-500 transition-all ease-in-out duration-300"
                 >
                   Popular
                 </div>
@@ -53,7 +68,7 @@ function IndexComponent() {
                     setSortMode("New");
                     setShowSortMenu(false);
                   }}
-                  className="cursor-pointer hover:text-cyan-500 transition-all ease-in-out duration-300"
+                  className="py-1 cursor-pointer hover:text-cyan-500 transition-all ease-in-out duration-300"
                 >
                   New
                 </div>
