b4f21cdcec14e41cc5bfe597975f26fda390148b
Paul K <102385105+codingcapy@users.noreply.github.com>
2025-12-13 10:53:11 -0800

    feat: add profile pic in comments and left border

--- frontend/src/components/CommentComponent.tsx
+++ frontend/src/components/CommentComponent.tsx
@@ -14,6 +14,9 @@ import { CommentNode, SerializedComment } from "../routes/posts/$postId";
 import { FaEllipsis } from "react-icons/fa6";
 import { CommentMenu } from "./CommentMenu";
 import usePostStore from "../store/PostStore";
+import { useQuery } from "@tanstack/react-query";
+import { getUserByIdQueryOptions } from "../lib/api/users";
+import defaultProfile from "/capypaul01.jpg";
 
 export function CommentComponent(props: { comment: CommentNode; post: Post }) {
   const [replyMode, setReplyMode] = useState(false);
@@ -29,6 +32,11 @@ export function CommentComponent(props: { comment: CommentNode; post: Post }) {
   const { editCommentModePointer, setEditCommentModePointer } = usePostStore();
   const { mutate: updateComment, isPending: updateCommentPending } =
     useUpdateCommentMutation();
+  const {
+    data: commenter,
+    isLoading: commenterLoading,
+    error: commenterError,
+  } = useQuery(getUserByIdQueryOptions(props.comment.userId));
 
   function handleCreateComment(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
@@ -71,7 +79,7 @@ export function CommentComponent(props: { comment: CommentNode; post: Post }) {
   return (
     <div
       key={props.comment.commentId}
-      className={`my-3`}
+      className={`relative my-3 flex`}
       style={{
         marginLeft:
           window.innerWidth > 760
@@ -79,159 +87,174 @@ export function CommentComponent(props: { comment: CommentNode; post: Post }) {
             : props.comment.level * 10,
       }}
     >
-      <div className="flex text-[#bdbdbd] text-sm">
-        <div className="font-bold">{props.comment.username}</div>
-        <div className="px-1">•</div>
-        <div>{displayDate(props.post.createdAt)}</div>
-      </div>
-      {editCommentModePointer === props.comment.commentId ? (
-        <form
-          onSubmit={handleEditComment}
-          className={`pl-5 py-2 my-5 rounded-2xl border border-[#5c5c5c] w-full hover:bg-[#383838] hover:border-[#818181] transition-all ease-in-out duration-300`}
-        >
-          <input
-            type="text"
-            placeholder="Add your reply"
-            className="outline-none w-full"
-            value={commentContent}
-            onChange={(e) => setCommentContent(e.target.value)}
-            required
-          />
-          <div className="flex justify-end">
-            <div
-              onClick={(e) => {
-                e.stopPropagation();
-                setEditCommentModePointer(0);
-              }}
-              className="cursor-pointer px-2 py-1 rounded-full bg-[#383838]"
-            >
-              Cancel
+      <img
+        src={
+          !commenterLoading || !commenterError
+            ? commenter
+              ? commenter.profilePic
+                ? commenter.profilePic
+                : defaultProfile
+              : defaultProfile
+            : defaultProfile
+        }
+        alt=""
+        className="absolute top-0 left-0 w-8 h-8 rounded-full"
+      />
+      <div className="ml-4 pl-5 border-l border-[#555555]">
+        <div className="flex text-[#bdbdbd] text-sm">
+          <div className="font-bold">{props.comment.username}</div>
+          <div className="px-1">•</div>
+          <div>{displayDate(props.post.createdAt)}</div>
+        </div>
+        {editCommentModePointer === props.comment.commentId ? (
+          <form
+            onSubmit={handleEditComment}
+            className={`pl-5 py-2 my-5 rounded-2xl border border-[#5c5c5c] w-full hover:bg-[#383838] hover:border-[#818181] transition-all ease-in-out duration-300`}
+          >
+            <input
+              type="text"
+              placeholder="Add your reply"
+              className="outline-none w-full"
+              value={commentContent}
+              onChange={(e) => setCommentContent(e.target.value)}
+              required
+            />
+            <div className="flex justify-end">
+              <div
+                onClick={(e) => {
+                  e.stopPropagation();
+                  setEditCommentModePointer(0);
+                }}
+                className="cursor-pointer px-2 py-1 rounded-full bg-[#383838]"
+              >
+                Cancel
+              </div>
+              <button
+                disabled={updateCommentPending}
+                className="mx-2 px-2 py-1 rounded-full bg-red-500 cursor-pointer"
+              >
+                {updateCommentPending ? "Saving..." : "Save Edits"}
+              </button>
             </div>
-            <button
-              disabled={updateCommentPending}
-              className="mx-2 px-2 py-1 rounded-full bg-red-500 cursor-pointer"
-            >
-              {updateCommentPending ? "Saving..." : "Save Edits"}
-            </button>
+          </form>
+        ) : (
+          <div>{props.comment.content}</div>
+        )}
+        <div className="flex">
+          <div className="mr-3">
+            <CommentVotesComponent post={props.post} comment={props.comment} />
           </div>
-        </form>
-      ) : (
-        <div>{props.comment.content}</div>
-      )}
-      <div className="flex">
-        <div className="mr-3">
-          <CommentVotesComponent post={props.post} comment={props.comment} />
-        </div>
-        <div
-          onClick={() => {
-            if (!user) navigate({ to: "/login" });
-            setCommentContent("");
-            setReplyMode(true);
-          }}
-          className="flex ml-2 cursor-pointer hover:text-cyan-500 transition-all ease-in-out duration-300"
-        >
-          <IoChatbubbleOutline size={22} />
-          <div className="ml-2">Reply</div>
-        </div>
-        {user && props.comment.userId === user.userId && (
-          <div className="relative">
-            <div
-              onClick={(e) => {
-                e.preventDefault();
-                e.stopPropagation();
-                setShowMenu(!showMenu);
-              }}
-              className="ml-3 px-2 pt-1.5 rounded-full hover:text-cyan-700 transition-all ease-in-out duration-300 cursor-pointer"
-            >
-              <FaEllipsis />
+          <div
+            onClick={() => {
+              if (!user) navigate({ to: "/login" });
+              setCommentContent("");
+              setReplyMode(true);
+            }}
+            className="flex ml-2 cursor-pointer hover:text-cyan-500 transition-all ease-in-out duration-300"
+          >
+            <IoChatbubbleOutline size={22} />
+            <div className="ml-2">Reply</div>
+          </div>
+          {user && props.comment.userId === user.userId && (
+            <div className="relative">
+              <div
+                onClick={(e) => {
+                  e.preventDefault();
+                  e.stopPropagation();
+                  setShowMenu(!showMenu);
+                }}
+                className="ml-3 px-2 pt-1.5 rounded-full hover:text-cyan-700 transition-all ease-in-out duration-300 cursor-pointer"
+              >
+                <FaEllipsis />
+              </div>
+              {showMenu && (
+                <CommentMenu
+                  comment={props.comment}
+                  setDeleteMode={setDeleteMode}
+                  setShowMenu={setShowMenu}
+                  setCommentContent={setCommentContent}
+                />
+              )}
             </div>
-            {showMenu && (
-              <CommentMenu
-                comment={props.comment}
-                setDeleteMode={setDeleteMode}
-                setShowMenu={setShowMenu}
-                setCommentContent={setCommentContent}
-              />
-            )}
+          )}
+        </div>
+        {replyMode && (
+          <form
+            onSubmit={handleCreateComment}
+            className={`pl-5 py-2 my-5 ${replyMode ? "rounded-2xl" : "rounded-full"} border border-[#5c5c5c] w-full hover:bg-[#383838] hover:border-[#818181] transition-all ease-in-out duration-300`}
+          >
+            <input
+              type="text"
+              placeholder="Add your reply"
+              className="outline-none w-full"
+              value={commentContent}
+              onChange={(e) => setCommentContent(e.target.value)}
+              required
+            />
+            <div className="flex justify-end">
+              <div
+                onClick={(e) => {
+                  e.stopPropagation();
+                  setReplyMode(false);
+                }}
+                className="cursor-pointer px-2 py-1 rounded-full bg-[#383838]"
+              >
+                Cancel
+              </div>
+              <button
+                disabled={createCommentPending}
+                className="mx-2 px-2 py-1 rounded-full bg-red-500 cursor-pointer"
+              >
+                {createCommentPending ? "Creating..." : "Comment"}
+              </button>
+            </div>
+          </form>
+        )}
+        {deleteMode && (
+          <div
+            className={`fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-[#222222] p-6 rounded shadow-lg w-[90%] max-w-md text-center z-100`}
+          >
+            <div className="text-2xl font-bold">Delete Comment?</div>
+            <div className="my-5">
+              Once you delete this comment, it can’t be restored.
+            </div>
+            <div className="my-5 flex justify-end">
+              <div
+                onClick={(e) => {
+                  e.stopPropagation();
+                  e.preventDefault();
+                  if (deleteCommentPending) return;
+                  deleteComment({ commentId: props.comment.commentId });
+                  setDeleteMode(false);
+                }}
+                className="p-2 mr-1 bg-red-500 rounded text-white bold secondary-font font-bold cursor-pointer"
+              >
+                {deleteCommentPending ? "Deleting..." : "DELETE"}
+              </div>
+              <div
+                onClick={(e) => {
+                  e.stopPropagation();
+                  e.preventDefault();
+                  setDeleteMode(false);
+                }}
+                className="p-2 ml-1 bg-[#5c5c5c] rounded bold secondary-font font-bold cursor-pointer"
+              >
+                CANCEL
+              </div>
+            </div>
           </div>
         )}
-      </div>
-      {replyMode && (
-        <form
-          onSubmit={handleCreateComment}
-          className={`pl-5 py-2 my-5 ${replyMode ? "rounded-2xl" : "rounded-full"} border border-[#5c5c5c] w-full hover:bg-[#383838] hover:border-[#818181] transition-all ease-in-out duration-300`}
-        >
-          <input
-            type="text"
-            placeholder="Add your reply"
-            className="outline-none w-full"
-            value={commentContent}
-            onChange={(e) => setCommentContent(e.target.value)}
-            required
+        {deleteMode && (
+          <div className="fixed inset-0 bg-black opacity-50 z-90"></div>
+        )}
+        {props.comment.children?.map((child) => (
+          <CommentComponent
+            key={child.commentId}
+            comment={child}
+            post={props.post}
           />
-          <div className="flex justify-end">
-            <div
-              onClick={(e) => {
-                e.stopPropagation();
-                setReplyMode(false);
-              }}
-              className="cursor-pointer px-2 py-1 rounded-full bg-[#383838]"
-            >
-              Cancel
-            </div>
-            <button
-              disabled={createCommentPending}
-              className="mx-2 px-2 py-1 rounded-full bg-red-500 cursor-pointer"
-            >
-              {createCommentPending ? "Creating..." : "Comment"}
-            </button>
-          </div>
-        </form>
-      )}
-      {deleteMode && (
-        <div
-          className={`fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-[#222222] p-6 rounded shadow-lg w-[90%] max-w-md text-center z-100`}
-        >
-          <div className="text-2xl font-bold">Delete Comment?</div>
-          <div className="my-5">
-            Once you delete this comment, it can’t be restored.
-          </div>
-          <div className="my-5 flex justify-end">
-            <div
-              onClick={(e) => {
-                e.stopPropagation();
-                e.preventDefault();
-                if (deleteCommentPending) return;
-                deleteComment({ commentId: props.comment.commentId });
-                setDeleteMode(false);
-              }}
-              className="p-2 mr-1 bg-red-500 rounded text-white bold secondary-font font-bold cursor-pointer"
-            >
-              {deleteCommentPending ? "Deleting..." : "DELETE"}
-            </div>
-            <div
-              onClick={(e) => {
-                e.stopPropagation();
-                e.preventDefault();
-                setDeleteMode(false);
-              }}
-              className="p-2 ml-1 bg-[#5c5c5c] rounded bold secondary-font font-bold cursor-pointer"
-            >
-              CANCEL
-            </div>
-          </div>
-        </div>
-      )}
-      {deleteMode && (
-        <div className="fixed inset-0 bg-black opacity-50 z-90"></div>
-      )}
-      {props.comment.children?.map((child) => (
-        <CommentComponent
-          key={child.commentId}
-          comment={child}
-          post={props.post}
-        />
-      ))}
+        ))}
+      </div>
     </div>
   );
 }
