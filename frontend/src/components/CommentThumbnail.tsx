723789cbd5e8c206369b1237860e411969cae115
PaulArkhet <102385105+codingcapy@users.noreply.github.com>
2026-01-18 09:18:10 -0800

    feat: update user profile comments profile picture

--- frontend/src/components/CommentThumbnail.tsx
+++ frontend/src/components/CommentThumbnail.tsx
@@ -8,6 +8,8 @@ import { useState } from "react";
 import { CommentMenu } from "./CommentMenu";
 import { useDeleteCommentMutation } from "../lib/api/comments";
 import { Comment } from "../../../schemas/comments";
+import { getUserByIdQueryOptions } from "../lib/api/users";
+import { useQuery } from "@tanstack/react-query";
 
 export function CommentThumbnail(props: { comment: Comment }) {
   const { user } = useAuthStore();
@@ -16,60 +18,67 @@ export function CommentThumbnail(props: { comment: Comment }) {
   const [commentContent, setCommentContent] = useState("");
   const { mutate: deleteComment, isPending: deleteCommentPending } =
     useDeleteCommentMutation();
+  const {
+    data: poster,
+    isLoading: posterLoading,
+    error: posterError,
+  } = useQuery(getUserByIdQueryOptions(props.comment.userId));
 
   return (
     <div
       key={props.comment.commentId}
       className="mx-auto w-full md:w-[50%] 2xl:w-[750px] border-t border-[#636363]"
     >
-      <Link
-        to="/posts/$postId"
-        params={{
-          postId: props.comment.postId.toString(),
-        }}
-        key={props.comment.postId}
-      >
-        <div className="relative my-1 rounded py-2 px-4 hover:bg-[#333333] transition-all ease-in-out duration-300">
-          <div className="flex justify-between">
-            <div className="flex text-[#bdbdbd] text-sm">
-              <img
-                src={
-                  user
-                    ? user.profilePic
-                      ? user.profilePic
-                      : defaultProfile
-                    : defaultProfile
-                }
-                alt=""
-                className="w-6 h-6 rounded-full"
+      {posterLoading ? (
+        <div>Loading...</div>
+      ) : posterError ? (
+        <div>Error loading poster</div>
+      ) : poster ? (
+        <Link
+          to="/posts/$postId"
+          params={{
+            postId: props.comment.postId.toString(),
+          }}
+          key={props.comment.postId}
+        >
+          <div className="relative my-1 rounded py-2 px-4 hover:bg-[#333333] transition-all ease-in-out duration-300">
+            <div className="flex justify-between">
+              <div className="flex text-[#bdbdbd] text-sm">
+                <img
+                  src={poster.profilePic ? poster.profilePic : defaultProfile}
+                  alt=""
+                  className="w-6 h-6 rounded-full"
+                />
+                <div className="font-bold ml-2">{poster.username}</div>
+                <div className="px-1">•</div>
+                <div>{displayDate(props.comment.createdAt)}</div>
+              </div>
+              <div
+                onClick={(e) => {
+                  e.stopPropagation();
+                  e.preventDefault();
+                  setShowMenu(!showMenu);
+                }}
+                className="absolute top-1 right-1 p-3 rounded-full hover:bg-[#575757] transition-all ease-in-out duration-300"
+              >
+                <FaEllipsis />
+              </div>
+            </div>
+            <div>{props.comment.content}</div>
+            <CommentVotesComponent comment={props.comment} />
+            {showMenu && (
+              <CommentMenu
+                comment={props.comment}
+                setDeleteMode={setDeleteMode}
+                setShowMenu={setShowMenu}
+                setCommentContent={setCommentContent}
               />
-              <div className="font-bold ml-2">{user && user.username}</div>
-              <div className="px-1">•</div>
-              <div>{displayDate(props.comment.createdAt)}</div>
-            </div>
-            <div
-              onClick={(e) => {
-                e.stopPropagation();
-                e.preventDefault();
-                setShowMenu(!showMenu);
-              }}
-              className="absolute top-1 right-1 p-3 rounded-full hover:bg-[#575757] transition-all ease-in-out duration-300"
-            >
-              <FaEllipsis />
-            </div>
+            )}
           </div>
-          <div>{props.comment.content}</div>
-          <CommentVotesComponent comment={props.comment} />
-          {showMenu && (
-            <CommentMenu
-              comment={props.comment}
-              setDeleteMode={setDeleteMode}
-              setShowMenu={setShowMenu}
-              setCommentContent={setCommentContent}
-            />
-          )}
-        </div>
-      </Link>
+        </Link>
+      ) : (
+        <div></div>
+      )}
       {deleteMode && (
         <div
           className={`fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-[#222222] p-6 rounded shadow-lg w-[90%] max-w-md text-center z-100`}
